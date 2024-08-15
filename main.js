4f81a6cd1a9f3605fdb1d39bed4b2924f0e09974
AsmaaAlzuriy1908 <samaa_zakaria712@icloud.com>
2024-08-15 16:23:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var userRole = window.prompt("enter your role")
+if (userRole==="ADMIN") {
+    console.log("you can rege");
+}
+else if (userRole==="sales") {
+console.log("you can rege");
+}
+else{
+    console.log("sorry");
+    
+}
---
+++ main2.js
@@ -0,0 +1,16 @@
+var superMar=window.prompt("enter your role")
+if (superMar=="superadmin") {
+    console.log("you can login as super admin");  
+}
+else if (superMar=="admin") {
+console.log("you can login as super admin");  
+}
+else if (superMar=="sales") {
+    console.log("you can login as super sales");  
+    }
+    else if (superMar=="markteer") {
+        console.log("you can login as super markter");  
+        }
+        else if (superMar=="moder") {
+            console.log("you can login as super moder");  
+            }
